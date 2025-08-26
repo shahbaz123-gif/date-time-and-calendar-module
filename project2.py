2d58656a65c939850c848e5ea94a1f011255f1a6
shahbaz123-gif <sajjadahmadjan30@gmail.com>
2025-08-26 19:28:09 +0500

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,8 @@
+months = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+# Display all months
+for month in months:
+    print(month)
